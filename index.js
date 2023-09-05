f7e0f7640e3826195e45ca0bbbc3a84bc5a50291
Joshua Okorie <joshuaokorie008@gmail.com>
2023-09-05 07:02:50 +0100

    changes

--- index.js
+++ index.js
@@ -16,20 +16,14 @@ app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
 
 
 
-app.use("/api/login", login);
+app.use("/login", login);
 
-app.use("/api/signup", signup);
+app.use("/signup", signup);
 
-app.use("/api/subscription", subscription);
+app.use("/subscription", subscription);
 
 app.post("/uploadproduct", async (req, res) => {
     const data = productModel(req.body);
